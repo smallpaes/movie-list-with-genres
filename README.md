7b7febb30bb41eb7898155f468c2765dff734165
Mike <mike.huang.mikank@gmail.com>
2019-05-19 19:55:33 +0800

    refine functions in JS, styling in CSS, and create README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Movie List with Genres
+A simple web application to display movies of various genres
+
+## Features
+- List down movies from Movie list API
+- Filter movies by genres
+
+### Filter Movie
+Choose the genre from genres list
--- main.js
+++ main.js
@@ -25,32 +25,23 @@ const genres = {
   "19": "Western"
 }
 
-//create MovieList constructor function
-function MovieList() { }
-
-//create instance
-const movieList = new MovieList()
-
-//add display genres method to MovieList
-MovieList.prototype.displayGenres = function (genresData) {
+function displayGenres(genresData) {
   let genresInnerHTML = ''
 
   for (let genre in genresData) {
     genresInnerHTML += `
-      <a class="nav-link border rounded-0" id="v-pills-${genresData[genre]}-tab" data-toggle="pill" href="#javascript:;" role="tab" aria-controls="v-pills-action"
+      <a class="nav-link border" id="v-pills-${genresData[genre]}-tab" data-toggle="pill" href="#javascript:;" role="tab" aria-controls="v-pills-action"
             aria-selected="false" data-genre-number=${genre}>${genresData[genre]}</a>
     `
   }
   genresDisplay.innerHTML = genresInnerHTML
 }
 
-//add get genres badges method to MovieList
-MovieList.prototype.getGenreBadges = function (movieGenres) {
+function getGenreBadges(movieGenres) {
   return movieGenres.map(genre => `<span class="badge badge-info mx-1">${genres[genre]}</span>`).join('')
 }
 
-//add display movies method to MovieList
-MovieList.prototype.displayMovies = function (data) {
+function displayMovies(data) {
   return moviesDisplay.innerHTML = data.map(movie => {
     const POSTER_URL = BASE_URL + '/posters/' + movie.image
     const movieGenres = movie.genres
@@ -61,7 +52,7 @@ MovieList.prototype.displayMovies = function (data) {
           <img src=${POSTER_URL} class="card-img-top" alt="Poster of ${movie.title}">
           <div class="card-body">
             <h6 class="card-title">${movie.title}</h6>
-              ${movieList.getGenreBadges(movieGenres)}
+              ${getGenreBadges(movieGenres)}
           </div>
         </div>
       </div>
@@ -69,27 +60,26 @@ MovieList.prototype.displayMovies = function (data) {
   }).join('')
 }
 
+function getGenreMovies(number) {
+  return data.filter(movie => movie.genres.indexOf(Number(number)) >= 0)
+}
+
 //get movie data from movie list API
 axios
   .get(INDEX_URL)
   .then(response => {
     data = response.data.results
-    movieList.displayGenres(genres)
-    movieList.displayMovies(data)
+    displayGenres(genres)
+    displayMovies(data)
   })
   .catch(error => console.log(error))
-
-//add get genre movies to MovieList
-MovieList.prototype.getGenreMovies = function (number) {
-  return data.filter(movie => movie.genres.indexOf(Number(number)) >= 0)
-}
 
 //add click event lister to genres list
 genresDisplay.addEventListener('click', event => {
   const genreNumber = event.target.dataset.genreNumber
-  const movieData = movieList.getGenreMovies(genreNumber)
+  const movieData = getGenreMovies(genreNumber)
 
   //display movies of this genre
-  movieList.displayMovies(movieData)
+  displayMovies(movieData)
 })
 
